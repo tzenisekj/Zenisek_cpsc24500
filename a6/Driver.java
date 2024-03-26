4366c8057e32c2b1b22f883f46b0b445288f70d7
Tyler Zenisek <tzenisekj@gmail.com>
2024-03-26 14:25:21 -0500

    added driver class to A6

---
+++ a6/Driver.java
@@ -0,0 +1,7 @@
+package a6;
+
+public class Driver {
+    Nodes n = new Nodes(); 
+
+    
+}
--- a6/Nodes.java
+++ a6/Nodes.java
@@ -1,6 +1,9 @@
 package a6;
 
-import java.util.ArrayList; 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+import a5.Node; 
 
 public class Nodes {
     private ArrayList<INode> list; 
@@ -9,9 +12,83 @@ public class Nodes {
         list = new ArrayList<INode>(); 
     }
 
+    public void fill(int size) throws Exception {
+        if (list.isEmpty())
+            list.clear();
+
+        Random r = new Random();
+		int numNodes = size + r.nextInt(size - 1);
+
+        for (int i = 0; i < numNodes; i++) {
+            list.add(NodeFactory.getNode()); 
+        }
+
+        for (int i = numNodes; i < size; i++) {
+            list.add(NodeFactory.getThreeDNode()); 
+        }
+    }
+
+    public int countNodes() {
+        int result = 0; 
+
+        if (list.isEmpty()) {
+            return result; 
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) instanceof Node) {
+                result += 1; 
+            }
+        }
+
+        return result; 
+    }
+
+    public int countThreeDNodes() {
+        int result = 0; 
+
+        if (list.isEmpty()) {
+            return result; 
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) instanceof ThreeDNode) {
+                result += 1; 
+            }
+        }
+
+        return result; 
+    }
+
     public void clear() {
         list.clear();
     }
 
-    
+    public String toString() {
+        String result = ""; 
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) instanceof ThreeDNode) {
+                result += String.valueOf(i + 1) + ". ThreeDNode -> " + list.get(i).toString() + "\n"; 
+            }
+            else {
+                result += String.valueOf(i + 1) + ". Node -> " + list.get(i).toString() + "\n";
+            }
+        }
+
+        return result; 
+    }
+
+    public void sort() {
+
+    }
+
+    private class Sorter implements Comparator {
+        @Override
+        public int compare(Object o1, Object o2) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'compare'");
+        }
+        
+    }
 }
